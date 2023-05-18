4d224304afddd0c11bdf28e12b49d269a7c93d87
Jacob Solomon <jmsolomon64@gmail.com>
2023-05-18 17:04:28 -0400

    Add project files.

---
+++ ApplicationTracker/ApplicationManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationTracker
+{
+    internal class ApplicationManager
+    {
+        public string Title { get; set; }
+        public decimal Salary { get; set; }
+        public DateTime Applied { get; set; }
+        public string Recruiter { get; set; }
+        public string Email { get; set; }
+        public bool Open { get; set; }
+        public List<FollowUp> FollowUps { get; set; }
+
+        internal class FollowUp
+        {
+            public string Description { get; set; }
+            public DateTime Arrived { get; set; }
+        }
+    }
+
+    
+}
